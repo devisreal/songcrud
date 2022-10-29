0bfab045c1e1d9764bf4b90beff703ac86ddfed5
Israel <akinosoissy@gmail.com>
2022-10-29 03:01:04 +0100

    Week 5 Python Task: Django CRUD

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Lyric)
+class LyricAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Song)
+class SongAdmin(admin.ModelAdmin):
+    pass
+
+
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artist = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
